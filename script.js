cce6d3bb8957ffdc92229f35fc1d581deb4ed81c
jonikramov <azizjonikramov1@gmail.com>
2023-02-18 16:45:11 +0500

    1. Часы и календарь

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+// 1. Часы и календарь
+
+
+
+function showTime() {
+    const time = document.querySelector('.time');
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString();
+    time.textContent = currentTime;
+    showDate()
+    setTimeout(showTime, 1000);
+  }
+
+showTime();
 
 
 
 
 
+function showDate() {
+    const date = document.querySelector('.date');
+    const date1 = new Date();
+    const options = {weekday: 'long', month: 'long', day: 'numeric'};
+    const currentDate = date1.toLocaleDateString('en-US', options);
+    date.textContent = currentDate;
+  }
 
-
-
-
-
-
+showDate();
 
 
 
